285e43c83d6741ef8e4595d8da26f7ac96794f1e
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-27 23:06:16 -0300

    em desenvolvimento - roteamento mongoDB
    npm install mongodb
    npm i cors

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cors from 'cors';
+import Videos from './dbModel.js';
 //App Config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -7,10 +9,31 @@ const connection_url =
   'mongodb+srv://admin:91537035Hugo@cluster0.vrklx63.mongodb.net/?retryWrites=true&w=majority';
 
 //Middleware
+app.use(express.json());
+app.use(Cors());
 
 //DB Config
 mongoose.connect(connection_url, {});
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
+
+app.post('/v2/posts', async (req, res) => {
+  try {
+    const dbVideos = req.body;
+    Videos.create(dbVideos);
+    res.status(201).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+app.get('/v2/posts', (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 //Listener
 app.listen(port, () => console.log(`Listening on localhost: ${port}`));
